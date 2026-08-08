a901db061f92221a5912d35c7fdbed8831549fe2
suman007dotcom <mrdey075@gmail.com>
2026-08-08 23:44:31 +0530

    filled inital infos

--- hand_tracking.py
+++ hand_tracking.py
@@ -2,8 +2,20 @@ import cv2
 import mediapipe as mp
 
 class Hand_tracker:
-    def __init__(self, mode=False, max_hands=2, detection_con=0.5, track_con=0.5):
+    def __init__(self, mode=False, max_hands=2, detection_con=0.5, track_con=0.5):        
+        self.mode = mode
+        self.max_hands = max_hands
+        self.detection_con = detection_con
+        self.track_con = track_con
 
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            static_image_mode=self.mode,
+            max_num_hands=self.max_hands,
+            min_detection_confidence=self.detection_con,
+            min_tracking_confidence=self.track_con
+        )
+        self.mp_draw = mp.solutions.drawing_utils
+        self.results = None
 
-        
     def find_hand(self,img,draw = True):
