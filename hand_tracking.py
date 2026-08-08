12a0ad3ee49f2916dfd3eee722d87759fb3795c9
suman007dotcom <mrdey075@gmail.com>
2026-08-08 20:24:33 +0530

    added structure

---
+++ hand_tracking.py
@@ -0,0 +1,9 @@
+import cv2
+import mediapipe as mp
+
+class Hand_tracker:
+    def __init__(self, mode=False, max_hands=2, detection_con=0.5, track_con=0.5):
+
+
+        
+    def find_hand(self,img,draw = True):
